656231f0f2e3b7dfb3b47a6b998cd307e5e5eeab
Kiash254 <codewithkiash@gmail.com>
2022-10-01 17:50:19 +0300

    solved a question based on an algorithm

--- intro.py
+++ intro.py
@@ -8,3 +8,22 @@ print('there are two types of data structures linear and non linear data structu
 print('linear data structrues data are arranged one after the other expamples are arrays,stack,Queue,linked list')
 
 print('non linear data structures-are arranged in a hierarchical manner where one element will be connected to one or more elements examples are graph,trees and many more')
+
+print(' Data structures are used to hold data while algorithms are used to solve the problem using that data.')
+print('Age group problem where i developed an algrithm to give a solution')
+age=int(input('Hello How old are you please....?'))
+
+if age<13:
+    print('the client is a small baby please we server from the age of 13')
+elif age<=18:
+    print('the client is a teenage')
+elif age<=35:
+    print('the client is  a youth')
+elif age<=45:
+    print('the client is a young parent')
+elif age<=60:
+    print('the client is an older parent')
+elif age<=75:
+    print('the client is a Grandpa consider with care')
+else:
+    print('this is an elgible client who is to be served for free')
